9604d83a53c702af7abf39b1de8a087c738c8db7
Raka Atmaja <rakapa23@gmail.com>
2023-06-03 12:40:17 +0700

    update

--- lib/auth/user.dart
+++ lib/auth/user.dart
@@ -1,19 +1,19 @@
 
 
-import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 
-class User extends InheritedWidget {
-  final GoogleSignInAccount? user;
+// class User extends InheritedWidget {
+//   final GoogleSignInAccount? user;
 
-  User({required this.user, required Widget child}) : super(child: child);
+//   User({required this.user, required Widget child}) : super(child: child);
 
-  static User? of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<User>();
-  }
+//   static User? of(BuildContext context) {
+//     return context.dependOnInheritedWidgetOfExactType<User>();
+//   }
 
-  @override
-  bool updateShouldNotify(User oldWidget) {
-    return user != oldWidget.user;
-  }
-}
+//   @override
+//   bool updateShouldNotify(User oldWidget) {
+//     return user != oldWidget.user;
+//   }
+// }
